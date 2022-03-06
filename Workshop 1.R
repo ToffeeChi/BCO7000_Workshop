da5c041d5664ddc98ff42c8f5840d2bd4ac69a60
迟程 <chicheng@192.168.20.131>
2022-03-06 17:01:23 +1100

    New file

---
+++ Workshop 1.R
@@ -0,0 +1,15 @@
+150/3
+a <- 10
+a
+b <- "Cool"
+c <- c(3,4,7,9)
+c
+summary(c)
+summary(c(3,4,7,9))
+install.packages("tidyverse")
+library(tidyverse)
+rm(b)
+data(mtcars)
+head(mtcars)
+summary(mtcars)
+glimpse(mtcars)
